c74271ea7389f9242212e92e902a6879cb3e809e
Matias Santillan <sntlln.93@gmail.com>
2024-05-05 03:53:20 -0300

    panel/Fix bad import

--- apps/panel/src/components/sidenav/nav.tsx
+++ apps/panel/src/components/sidenav/nav.tsx
@@ -1,7 +1,7 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
 import { NavProps } from '../navigation-links';
+import { buttonVariants } from '../ui/button-variants';
 
 export function Nav({ links, isCollapsed }: NavProps) {
   const { pathname } = { pathname: 'false' };
@@ -9,8 +9,7 @@ export function Nav({ links, isCollapsed }: NavProps) {
   return (
     <div
       data-collapsed={isCollapsed}
-      className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2"
-    >
+      className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2">
       <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
         {links.map((link, index) =>
           isCollapsed ? (
@@ -25,8 +24,7 @@ export function Nav({ links, isCollapsed }: NavProps) {
                     }),
                     'h-9 w-9',
                     'dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white'
-                  )}
-                >
+                  )}>
                   <link.icon className="h-4 w-4" />
                   <span className="sr-only">{link.title}</span>
                 </a>
@@ -47,8 +45,7 @@ export function Nav({ links, isCollapsed }: NavProps) {
                 }),
                 'dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white',
                 'justify-start'
-              )}
-            >
+              )}>
               <link.icon className="mr-2 h-4 w-4" />
               {link.title}
               {link.label && (
