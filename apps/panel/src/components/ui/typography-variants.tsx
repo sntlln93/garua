de427d7f46568e6db4581ff8673386c7675f10b1
Matias Santillan <sntlln.93@gmail.com>
2024-05-05 22:11:50 -0300

    panel/adjust heading size

--- apps/panel/src/components/ui/typography-variants.tsx
+++ apps/panel/src/components/ui/typography-variants.tsx
@@ -3,7 +3,7 @@ import { cva } from 'class-variance-authority';
 export const headingVariants = cva(' tracking-tight', {
   variants: {
     variant: {
-      h1: 'scroll-m-20 text-4xl font-extrabold lg:text-5xl',
+      h1: 'scroll-m-20 text-2xl font-extrabold',
       h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold first:mt-0',
       h3: 'scroll-m-20 text-2xl font-semibold',
       h4: 'scroll-m-20 text-xl font-semibold',
