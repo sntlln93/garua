393b15611e18b4c18ed878461934a9ee733793ad
Matias Santillan <sntlln.93@gmail.com>
2024-05-05 22:14:22 -0300

    panel/move to separate file for fast refresh

--- apps/panel/src/lib/hooks/use-media-query.tsx
+++ apps/panel/src/lib/hooks/use-media-query.tsx
@@ -1,22 +1,6 @@
 import { useLayoutEffect, useState } from 'react';
-
-type BreakPoint = 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-
-export function getBreakPoint(): BreakPoint {
-  const width = window.innerWidth;
-
-  if (width >= 1536) {
-    return '2xl';
-  } else if (width >= 1280) {
-    return 'xl';
-  } else if (width >= 1024) {
-    return 'lg';
-  } else if (width >= 768) {
-    return 'md';
-  } else {
-    return 'sm';
-  }
-}
+import { getBreakPoint } from '../utils';
+import { BreakPoint } from '../types';
 
 export const useMediaQuery = (): BreakPoint => {
   const [breakpoint, setBreakpoint] = useState<BreakPoint>(getBreakPoint());
--- apps/panel/src/lib/utils.ts
+++ apps/panel/src/lib/utils.ts
@@ -1,6 +1,23 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { BreakPoint } from './types';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getBreakPoint(): BreakPoint {
+  const width = window.innerWidth;
+
+  if (width >= 1536) {
+    return '2xl';
+  } else if (width >= 1280) {
+    return 'xl';
+  } else if (width >= 1024) {
+    return 'lg';
+  } else if (width >= 768) {
+    return 'md';
+  } else {
+    return 'sm';
+  }
 }
