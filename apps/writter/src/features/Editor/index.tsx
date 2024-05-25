2f29283143f7ec8f1f923007a42b7dd449bab296
Matias Santillan <sntlln.93@gmail.com>
2024-05-25 19:31:28 -0300

    rename panel app to writter

---
+++ apps/writter/src/features/Editor/index.tsx
@@ -0,0 +1,6 @@
+import { cn } from '@/lib/utils';
+import { headingVariants } from '@/components/ui/typography-variants';
+
+export function Editor() {
+  return <div className="flex flex-col justify-center gap-4"></div>;
+}
--- apps/writter/src/main.tsx
+++ apps/writter/src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import EditorProvider from './features/Editor/components/BodyEditor/context.tsx';
 import { TooltipProvider } from './components/ui/tooltip.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TooltipProvider>
-      <EditorProvider>
-        <App />
-      </EditorProvider>
+      <App />
     </TooltipProvider>
   </React.StrictMode>
 );
